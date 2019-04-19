40855d28fc8a0ead172e86865eb2fb37497e1686
Tássio Jordão <tassio72@hotmail.com>
2019-04-19 11:30:15 -0300

    Inserindo Score, result e controle do jogo

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 new Vue ({
     el: '#app',
     data: {
-
+        playerLife: 0,
+        monsterLife: 10,
+        //start game
+        running: false
     },
     computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
 
     },
     methods: {
